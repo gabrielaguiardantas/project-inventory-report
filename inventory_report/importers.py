3bad3280bb4541bf30df8d9480884343bd3faa71
Gabriel Aguiar Dantas <punkes@pop-os.localdomain>
2023-07-18 16:33:19 -0300

    req. 3, 4 e 8 ok

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,16 +1,34 @@
-from typing import Dict, Type
+import csv
+import json
+from abc import ABC, abstractmethod
+from typing import Dict, List, Type
+
+from inventory_report.product import Product
 
 
-class Importer:
-    pass
+class Importer(ABC):
+    def __init__(self, path: str) -> None:
+        self.path = path
+
+    @abstractmethod
+    def import_data(self) -> List[Product]:
+        ...
 
 
-class JsonImporter:
-    pass
+class JsonImporter(Importer):
+    def import_data(self) -> List[Product]:
+        with open(self.path) as file:
+            products = json.load(file)
+            new_list_products = [Product(**product) for product in products]
+        return new_list_products
 
 
-class CsvImporter:
-    pass
+class CsvImporter(Importer):
+    def import_data(self) -> List[Product]:
+        with open(self.path, encoding="utf-8") as file:
+            products = csv.DictReader(file)
+            new_list_products = [Product(**product) for product in products]
+        return new_list_products
 
 
 # Não altere a variável abaixo

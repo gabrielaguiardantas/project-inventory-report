1d801322cbc952b0279dfe63fac7612f888ef23b
Gabriel Aguiar Dantas <punkes@pop-os.localdomain>
2023-07-18 15:37:24 -0300

    req. 1, 2, ok

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,2 +1,23 @@
+from inventory_report.product import Product
+
+
 def test_create_product() -> None:
-    raise NotImplementedError
+    Mock_product_with_all_fields = Product(
+        id="1",
+        product_name="AI Dev Bot",
+        company_name="Developer-machine",
+        manufacturing_date="2021-01-01",
+        expiration_date="2021-03-31",
+        serial_number="123456789",
+        storage_instructions="Keep caution! The product could be a fraud",
+    )
+    assert Mock_product_with_all_fields.id == "1"
+    assert Mock_product_with_all_fields.product_name == "AI Dev Bot"
+    assert Mock_product_with_all_fields.company_name == "Developer-machine"
+    assert Mock_product_with_all_fields.manufacturing_date == "2021-01-01"
+    assert Mock_product_with_all_fields.expiration_date == "2021-03-31"
+    assert Mock_product_with_all_fields.serial_number == "123456789"
+    assert (
+        Mock_product_with_all_fields.storage_instructions
+        == "Keep caution! The product could be a fraud"
+    )
--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,22 @@
+from inventory_report.product import Product
+
+
 def test_product_report() -> None:
-    raise NotImplementedError
+    Mock_product_with_all_fields = Product(
+        id="1",
+        product_name="AI Dev Bot",
+        company_name="Developer-machine",
+        manufacturing_date="2021-01-01",
+        expiration_date="2021-03-31",
+        serial_number="123456789",
+        storage_instructions="Keep caution! The product could be a fraud",
+    )
+    assert str(Mock_product_with_all_fields) == (
+        "The product 1 - AI Dev Bot "
+        + "with serial number 123456789 "
+        + "manufactured on 2021-01-01 "
+        + "by the company Developer-machine "
+        + "valid until 2021-03-31 "
+        + "must be stored according to the following instructions: "
+        + "Keep caution! The product could be a fraud."
+    )

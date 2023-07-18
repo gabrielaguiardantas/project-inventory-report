de7f5053efc84e668a449d7f76eee5c1561959ba
Gabriel Aguiar Dantas <punkes@pop-os.localdomain>
2023-07-18 17:59:07 -0300

    req. 5, 6 ok

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,15 @@
+from typing import List, Union
+
+from inventory_report.product import Product
+
+
 class Inventory:
-    pass
+    def __init__(self, data: Union[List[Product], None] = None) -> None:
+        self._data = [] if data is None else data
+
+    @property
+    def data(self) -> List[Product]:
+        return self._data
+
+    def add_data(self, data: List[Product]) -> None:
+        self._data.extend(data)
--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,25 @@
-class SimpleReport:
-    pass
+from typing import List
+
+from inventory_report.inventory import Inventory
+from inventory_report.reports.report import Report
+
+
+class SimpleReport(Report):
+    def __init__(self) -> None:
+        self._inventory_list: List[Inventory] = []
+
+    def add_inventory(self, inventory: Inventory) -> None:
+        self._inventory_list.append(inventory)
+
+    # def generate(self) -> str:
+    #     report = (
+    #         "Data de fabricação mais antiga: "
+    #         + self._get_earliest_date()
+    #         + "\n"
+    #         + "Data de validade mais próxima: "
+    #         + self._get_closest_expiration_date()
+    #         + "\n"
+    #         + "Empresa com maior quantidade de produtos estocados: "
+    #         + self._get_company_with_most_stocked_products()
+    #     )
+    #     return report

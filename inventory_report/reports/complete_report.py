6a8d5e4d0c3dd88fe27b0ef30c737c14486c7482
Gabriel Aguiar Dantas <punkes@pop-os.localdomain>
2023-07-19 20:03:57 -0300

    req. 7, 9 ok

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,21 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def generate(self) -> str:
+        return (
+            super().generate()
+            + "\n"
+            + "Stocked products by company:\n"
+            + self.__convert_company_list_with_stocked_products_to_string()
+        )
+
+    def __convert_company_list_with_stocked_products_to_string(self) -> str:
+        complete_str = ""
+        for k, v in self._get_company_list_with_stocked_products():
+            complete_str += str(f"- {k}: {v}\n")
+
+        return complete_str
--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,4 +1,5 @@
-from typing import List
+from datetime import date
+from typing import List, Tuple
 
 from inventory_report.inventory import Inventory
 from inventory_report.reports.report import Report
@@ -11,15 +12,53 @@ class SimpleReport(Report):
     def add_inventory(self, inventory: Inventory) -> None:
         self._inventory_list.append(inventory)
 
-    # def generate(self) -> str:
-    #     report = (
-    #         "Data de fabricação mais antiga: "
-    #         + self._get_earliest_date()
-    #         + "\n"
-    #         + "Data de validade mais próxima: "
-    #         + self._get_closest_expiration_date()
-    #         + "\n"
-    #         + "Empresa com maior quantidade de produtos estocados: "
-    #         + self._get_company_with_most_stocked_products()
-    #     )
-    #     return report
+    def generate(self) -> str:
+        (
+            company_name,
+            total_stocked,
+        ) = self._get_company_list_with_stocked_products()[0]
+        report = (
+            "Oldest manufacturing date: "
+            + self._get_oldest_date()
+            + "\n"
+            + "Closest expiration date: "
+            + self._get_closest_expiration_date()
+            + "\n"
+            + "Company with the largest inventory: "
+            + company_name
+        )
+        return report
+
+    def _get_oldest_date(self) -> str:
+        oldest_date = self._inventory_list[0].data[0].manufacturing_date
+        for inventory in self._inventory_list:
+            for product in inventory.data:
+                # bigger date comes after smaller date
+                if product.manufacturing_date < oldest_date:
+                    oldest_date = product.manufacturing_date
+        return oldest_date
+
+    def _get_closest_expiration_date(self) -> str:
+        future_expiration_date_list = []
+        for inventory in self._inventory_list:
+            for product in inventory.data:
+                if product.expiration_date > str(date.today()):
+                    future_expiration_date_list.append(product.expiration_date)
+        return min(future_expiration_date_list)
+
+    def _get_company_list_with_stocked_products(
+        self,
+    ) -> List[Tuple[str, int]]:
+        company_name_frequency_dict: dict[str, int] = {}
+        for inventory in self._inventory_list:
+            for product in inventory.data:
+                company_name_frequency_dict[product.company_name] = (
+                    company_name_frequency_dict.get(product.company_name, 0)
+                    + 1
+                )
+        ordered_company_name_frequency_list = sorted(
+            company_name_frequency_dict.items(),
+            reverse=True,
+            key=lambda kv: (kv[1], kv[0]),
+        )
+        return ordered_company_name_frequency_list
